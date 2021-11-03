ec4758617f227be51f3b307026f5c501de2cc417
sieunlee <dltldmsm@gmail.com>
2021-11-03 20:06:07 +0900

    211103

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
     </View>
   );
 }
