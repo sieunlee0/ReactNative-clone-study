c45754c20a7503ea58d2c766c1450bc5f611db4c
sieunlee <dltldmsm@gmail.com>
2021-11-03 20:08:25 +0900

    211103

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View>
-        <Text>Hello!</Text>
+        <Text>Gyeonggi-do</Text>
       </View>
       <StatusBar style="light" />
     </View>
