0612a771209f2b223a7e5ad8201b2b8e9e2c5f27
sieunlee <dltldmsm@gmail.com>
2021-11-05 15:50:10 +0900

    211105

--- App.js
+++ App.js
@@ -4,28 +4,51 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{flex:1}}>
-      <View style={{flex:1, backgroundColor: "salmon"}}></View>
-      <View style={{flex:1, backgroundColor: "teal"}}></View>
-      <View style={{flex:1, backgroundColor: "orange"}}></View>
+    <View style={styles.container}>
       <StatusBar style="light" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Gyeonggi-do</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27°</Text>
+          <Text style={styles.description}>Cloudy</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  //   backgroundColor: 'salmon',
-  //   alignItems: 'center',
-  //   justifyContent: 'center',
-  // },
-  // city: {
-  //   flex: 1,
-  //   backgroundColor: "white",
-  // },
-  // weather: {
-  //   flex: 1,
-  //   backgroundColor: "yellow",
-  // },
+  container: {
+    flex: 1,
+    backgroundColor: 'teal',
+    alignItems: 'center',
+  },
+  city: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: "space-around",
+  },
+  cityName: {
+    color: "white",
+    fontSize: 40,
+    fontWeight: "600",
+  },
+  weather: {
+    flex: 2,
+    backgroundColor: "white",
+    
+  },
+  day: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  temp: {
+
+  },
+  description: {
+
+  },
 });
